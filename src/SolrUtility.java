04a4e3e6e7cb8ced53e39ebdc043380ceec1010d
mayurimehrotra <mayurime@usc.edu>
2016-04-02 12:29:31 -0700

    Added SolrUtility

---
+++ src/SolrUtility.java
@@ -0,0 +1,27 @@
+import java.io.IOException;
+
+public class SolrUtility {
+
+	public static void main(String[] args) {
+	
+		
+		String cmd1="/home/mayuri/solr-4.10.0/bin/solr start";
+		String fileName= "books.json";
+		String cmd2= "curl 'http://localhost:8983/solr/test/update?commit=true' --data-binary @/media/mayuri/Data/" +fileName+ " -H 'Content-type:application/json' > SolrOut.txt ";
+		String cmd3="/home/mayuri/solr-4.10.0/bin/solr restart";
+		
+		ProcessBuilder pb1=new ProcessBuilder("bash","-c",cmd1);
+		ProcessBuilder pb2=new ProcessBuilder("bash","-c",cmd2);
+		ProcessBuilder pb3=new ProcessBuilder("bash","-c",cmd3);
+		
+		try {
+			//pb1.start();
+			pb3.start();
+			pb2.start();
+			System.out.println("Done");		
+		} catch (IOException e) {			
+			e.printStackTrace();
+		}
+		
+	}	
+}
